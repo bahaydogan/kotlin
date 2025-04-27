712e631c5b2fa7b4ca3efe3d455ff9396e0224a9
Baha Aydogan <bahaydogann@gmail.com>
2025-04-27 15:41:54 +0300

    update deneme.kt

--- deneme.kt
+++ deneme.kt
@@ -1,21 +1,34 @@
-package com.example.untitled
-
 fun main() {
-
+    // Temel Veri Tipleri
     val sehir: String = "İzmir"
     val ulke: String = "Türkiye"
     val telefon: String = "+90 5XX XXX XX XX"
     val postaKodu: String = "35000"
     val email: String = "example@email.com"
     val meslek: String = "Mühendis"
-    var stokMiktari: Int = 100 // Use 'var' if the value might change
+    var stokMiktari: Int = 100
     val musteriAdi: String = "Ayşe Demir"
     val bakiye: Double = 150.75
     val dogumGunu: String = "1990-05-15"
     val maas: Double = 55000.0
     val medeniDurum: String = "Bekar"
     val urunYorum: String = "Harika bir ürün!"
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("E-posta: $email")
+    println("Meslek: $meslek")
+    println("Stok Miktarı: $stokMiktari")
+    println("Müşteri Adı: $musteriAdi")
+    println("Bakiye: $bakiye")
+    println("Doğum Günü: $dogumGunu")
+    println("Maaş: $maas")
+    println("Medeni Durum: $medeniDurum")
+    println("Ürün Yorumu: $urunYorum")
+    println("---")
 
+    // Tarih ve Sayısal Veriler
     val odemeTarihi: String = "2025-05-05"
     val odeme: Double = 75.99
     val siparisAdeti: Int = 2
@@ -28,19 +41,46 @@ fun main() {
     val boylam: Double = 27.1287
     val urunAdi: String = "Akıllı Telefon"
     val yemekFiyati: Double = 25.50
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme Miktarı: $odeme")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("İndirim Miktarı: $indirimMiktari")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+    println("Ürün Adı: $urunAdi")
+    println("Yemek Fiyatı: $yemekFiyati")
+    println("---")
 
+    // Diğer Tanımlamalar
     val marka: String = "Samsung"
     val muzikAdi: String = "Bohemian Rhapsody"
-    val videoSuresi: Double = 5.55 // in minutes
+    val videoSuresi: Double = 5.55
     val urunPuani: Double = 4.8
     val resimAdi: String = "manzara.jpg"
     val dosyaFormati: String = "JPEG"
     val renk: String = "Mavi"
     val renkKodu: String = "#0000FF"
     val telefonModeli: String = "Galaxy S21"
-    val ekranBoyutu: Double = 6.2 // in inches
-    val agirlik: Double = 170.0 // in grams
+    val ekranBoyutu: Double = 6.2
+    val agirlik: Double = 170.0
+    println("Marka: $marka")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi (dakika): $videoSuresi")
+    println("Ürün Puanı: $urunPuani")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu (inç): $ekranBoyutu")
+    println("Ağırlık (gram): $agirlik")
+    println("---")
 
+    // Tatil ve Adres Bilgileri
     val ulusalGun: String = "2025-04-23"
     val tatilGunu: String = "2025-05-01"
     val rezervasyonTarihi: String = "2025-05-10"
@@ -51,5 +91,14 @@ fun main() {
     val kuponSuresi: String = "2025-05-31"
     val kuponKodu: String = "SUMMER20"
     val faturaAdresi: String = "Örnek Mah. Örnek Sok. No:12 Daire:3"
-
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
 }
