78b143e1c6934e58252ca6a72a0d82231e02ac31
Baha Aydogan <bahaydogann@gmail.com>
2025-04-27 15:27:08 +0300

    Add only deneme.kt

---
+++ deneme.kt
@@ -0,0 +1,55 @@
+package com.example.untitled
+
+fun main() {
+
+    val sehir: String = "İzmir"
+    val ulke: String = "Türkiye"
+    val telefon: String = "+90 5XX XXX XX XX"
+    val postaKodu: String = "35000"
+    val email: String = "example@email.com"
+    val meslek: String = "Mühendis"
+    var stokMiktari: Int = 100 // Use 'var' if the value might change
+    val musteriAdi: String = "Ayşe Demir"
+    val bakiye: Double = 150.75
+    val dogumGunu: String = "1990-05-15"
+    val maas: Double = 55000.0
+    val medeniDurum: String = "Bekar"
+    val urunYorum: String = "Harika bir ürün!"
+
+    val odemeTarihi: String = "2025-05-05"
+    val odeme: Double = 75.99
+    val siparisAdeti: Int = 2
+    val arabaModeli: String = "Sedan"
+    val kitapAdi: String = "Yüz Yıllık Yalnızlık"
+    val yayinlamaTarihi: String = "1967"
+    val indirimMiktari: Double = 10.50
+    val odaSayisi: Int = 3
+    val enlem: Double = 38.4189
+    val boylam: Double = 27.1287
+    val urunAdi: String = "Akıllı Telefon"
+    val yemekFiyati: Double = 25.50
+
+    val marka: String = "Samsung"
+    val muzikAdi: String = "Bohemian Rhapsody"
+    val videoSuresi: Double = 5.55 // in minutes
+    val urunPuani: Double = 4.8
+    val resimAdi: String = "manzara.jpg"
+    val dosyaFormati: String = "JPEG"
+    val renk: String = "Mavi"
+    val renkKodu: String = "#0000FF"
+    val telefonModeli: String = "Galaxy S21"
+    val ekranBoyutu: Double = 6.2 // in inches
+    val agirlik: Double = 170.0 // in grams
+
+    val ulusalGun: String = "2025-04-23"
+    val tatilGunu: String = "2025-05-01"
+    val rezervasyonTarihi: String = "2025-05-10"
+    val sokakAdi: String = "Atatürk Caddesi"
+    val otobusHatti: String = "910"
+    val kalanDakika: Int = 30
+    val takipKodu: String = "ABC123XYZ"
+    val kuponSuresi: String = "2025-05-31"
+    val kuponKodu: String = "SUMMER20"
+    val faturaAdresi: String = "Örnek Mah. Örnek Sok. No:12 Daire:3"
+
+}
